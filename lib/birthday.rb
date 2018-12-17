8d93a27f138598e90b45bae8ddd62b88f7f8c516
Alua Baikadamova <abaikad00@gmail.com>
2018-12-17 16:37:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,19 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  
+ 
+  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner  
+
+  
 end
 
 
